2ea0763f6d04c6085f580fc2d1e4c4600f8386d3
cielspride <44986577+cielspride@users.noreply.github.com>
2018-12-05 01:05:44 -0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,35 @@
+    </main>            
+            <footer class="container-fluid nopadding COLORE fontecatorze">
+                <section class="row">
+                    <div class="col-md-3 marginfooter">
+                    <p>
+                        Central de Atendimento:</br>
+                        Telefone: (000) 0000-0000</br>
+                        E-mail: livrariavolare@mail.com
+                    </p>
+                    </div>
+                    <div class="col-md-3 marginfooter">
+                    <p>
+                        Forma de Pagamento:<br/>
+                        <img src="img/pagsegurologo.png" title="logomarca" alt="logo livraria volare">
+
+                    </p>
+                    </div>
+                    <div class="col-md-3 marginfooter">
+                    <p >
+                        Siga-nos nas redes:<br/>
+                        <span style="font-size: 3rem;"><i class="fab fa-facebook"></i></span>&nbsp;
+                        <span style="font-size: 3rem;"><i class="fab fa-instagram"></i></span>
+                    </p>
+                    </div>
+                    <div class="col-md-2 paddingtop">
+                        <img src="img/logo2.jpg" alt="logo volare">
+                    </div>
+                </section>    
+            </footer>
+        <!-- javascript -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        </body>
+</html>
